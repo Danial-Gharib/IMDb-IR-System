cc0799005a77a8c28a57ad5d06c675fa7f3669ec
Danial Gharib <danial.mail.gh@gmail.com>
2024-04-10 19:31:30 +0330

    index.py completed

--- Logic/core/indexer/index.py
+++ Logic/core/indexer/index.py
@@ -3,8 +3,13 @@ import os
 import json
 import copy
 from indexes_enum import Indexes
-
-
+### importing preprocessor
+import sys
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+from preprocess import Preprocessor
+####
 class Index:
     def __init__(self, preprocessed_documents: list):
         """
@@ -33,7 +38,8 @@ class Index:
 
         current_index = {}
         #         TODO
-
+        for doc in self.preprocessed_documents:
+            current_index[doc['id']] = doc
         return current_index
 
     def index_stars(self):
@@ -48,6 +54,13 @@ class Index:
         """
 
         #         TODO
+        current_index = {}
+        for doc in self.preprocessed_documents:
+            for star in doc.get('stars', []):
+                if star not in current_index:
+                    current_index[star] = {}
+                current_index[star][doc['id']] = doc['stars'].count(star)
+        return current_index
         pass
 
     def index_genres(self):
@@ -62,6 +75,13 @@ class Index:
         """
 
         #         TODO
+        current_index = {}
+        for doc in self.preprocessed_documents:
+            for genre in doc.get('genres', []):
+                if genre not in current_index:
+                    current_index[genre] = {}
+                current_index[genre][doc['id']] = doc['genres'].count(genre)
+        return current_index
         pass
 
     def index_summaries(self):
@@ -77,7 +97,11 @@ class Index:
 
         current_index = {}
         #         TODO
-
+        for doc in self.preprocessed_documents:
+            for word in doc.get('summaries', []):
+                if word not in current_index:
+                    current_index[word] = {}
+                current_index[word][doc['id']] = doc['summaries'].count(word)
         return current_index
 
     def get_posting_list(self, word: str, index_type: str):
@@ -99,6 +123,7 @@ class Index:
 
         try:
             #         TODO
+            return list(self.index[index_type][word].keys())
             pass
         except:
             return []
@@ -114,6 +139,20 @@ class Index:
         """
 
         #         TODO
+        self.index[Indexes.DOCUMENTS.value][document['id']] = document
+        for star in document.get('stars', []):
+            if star not in self.index[Indexes.STARS.value]:
+                self.index[Indexes.STARS.value][star] = {}
+            self.index[Indexes.STARS.value][star][document['id']] = document['stars'].count(star)
+        for genre in document.get('genres', []):
+            if genre not in self.index[Indexes.GENRES.value]:
+                self.index[Indexes.GENRES.value][genre] = {}
+            self.index[Indexes.GENRES.value][genre][document['id']] = document['genres'].count(genre)
+        for word in document.get('summaries', []):
+            if word not in self.index[Indexes.SUMMARIES.value]:
+                self.index[Indexes.SUMMARIES.value][word] = {}
+            self.index[Indexes.SUMMARIES.value][word][document['id']] = document['summaries'].count(word)
+        return        
         pass
 
     def remove_document_from_index(self, document_id: str):
@@ -125,7 +164,17 @@ class Index:
         document_id : str
             ID of the document to remove from all the indexes
         """
-
+        if document_id in self.index[Indexes.DOCUMENTS.value]:
+            del self.index[Indexes.DOCUMENTS.value][document_id]
+        for star_index in self.index[Indexes.STARS.value].values():
+            if document_id in star_index:
+                del star_index[document_id]
+        for genre_index in self.index[Indexes.GENRES.value].values():
+            if document_id in genre_index:
+                del genre_index[document_id]
+        for summary_index in self.index[Indexes.SUMMARIES.value].values():
+            if document_id in summary_index:
+                del summary_index[document_id]
         #         TODO
         pass
 
@@ -148,7 +197,6 @@ class Index:
         if index_after_add[Indexes.DOCUMENTS.value]['100'] != dummy_document:
             print('Add is incorrect, document')
             return
-
         if (set(index_after_add[Indexes.STARS.value]['tim']).difference(set(index_before_add[Indexes.STARS.value]['tim']))
                 != {dummy_document['id']}):
             print('Add is incorrect, tim')
@@ -202,6 +250,9 @@ class Index:
             raise ValueError('Invalid index name')
 
         # TODO
+        with open(os.path.join(path, f'{index_name}_index.json'), 'w') as f:
+            json.dump(self.index[index_name], f)
+        return
         pass
 
     def load_index(self, path: str):
@@ -213,7 +264,12 @@ class Index:
         path : str
             Path to load the file
         """
-
+        for index_type in Indexes:
+            file_path = os.path.join(path, f'{index_type.value}_index.json')
+            if os.path.exists(file_path):
+                with open(file_path, 'r') as f:
+                    self.index[index_type.value] = json.load(f)
+        return    
         #         TODO
         pass
 
@@ -269,7 +325,6 @@ class Index:
             # if we have found 3 documents with the word, we can break
             if len(docs) == 3:
                 break
-
         end = time.time()
         brute_force_time = end - start
 
@@ -298,3 +353,64 @@ class Index:
             return False
 
 # TODO: Run the class with needed parameters, then run check methods and finally report the results of check methods
+def main():
+    curr_dir = os.path.dirname(__file__)
+    parent_dir = os.path.dirname(curr_dir)
+    grandparent_dir = os.path.dirname(parent_dir)
+    grandgrand_dir = os.path.dirname(grandparent_dir)
+    with open (os.path.join(grandgrand_dir, 'IMDB_crawled.json'), 'r') as f:
+        imdb_data = json.load(f)
+    ###############
+    ids = []
+    summareis = []
+    stars = []
+    genres = []
+    for movie in imdb_data:
+        if movie['summaries'] != 'not-present' and movie['stars'] != 'not-present' and movie['genres'] != 'not-present':
+            ids.append(movie['id'])
+            summareis.append(' '.join(movie['summaries']))
+            stars.append(' '.join(movie['stars']))
+            genres.append(' '.join(movie['genres']))
+
+
+    print("Start preprocessing")
+    pp_sum = Preprocessor(summareis, stopwords_path='stopwords.txt', does_lemmatize=False)
+    preprocessed_summaries = pp_sum.preprocess()
+    pp_stars = Preprocessor(stars, stopwords_path='stopwords.txt', does_lemmatize=False, does_stem=False)
+    preprocessed_stars = pp_stars.preprocess()
+    pp_genres = Preprocessor(genres, stopwords_path='stopwords.txt', does_lemmatize=False, does_stem=False)
+    preprocessed_genres = pp_genres.preprocess()
+    print("preprocessing finished")
+    ##############
+    preprocessed_docs = []
+    for movie_idx in range(len(ids)):
+        movie = {
+            'id':ids[movie_idx],
+            'summaries':preprocessed_summaries[movie_idx],
+            'stars':preprocessed_stars[movie_idx],
+            'genres':preprocessed_genres[movie_idx]
+        }
+        preprocessed_docs.append(movie)
+    ##### now testing the indexes :
+    indexer = Index(preprocessed_documents=preprocessed_docs)
+
+    indexer.check_add_remove_is_correct()
+
+    indexer.check_if_indexing_is_good('summaries', 'good')
+    indexer.check_if_indexing_is_good('stars', 'clint')
+    indexer.check_if_indexing_is_good('genres', 'crime')
+
+
+    indexer.store_index('indexes', 'documents')
+    indexer.store_index('indexes', 'stars')
+    indexer.store_index('indexes', 'summaries')
+    indexer.store_index('indexes', 'genres')
+    ### check if index loaded correctly for different index types 
+
+    print(indexer.check_if_index_loaded_correctly('documents', indexer.index['documents']))
+    print(indexer.check_if_index_loaded_correctly('stars', indexer.index['stars']))
+    print(indexer.check_if_index_loaded_correctly('genres', indexer.index['genres']))
+    print(indexer.check_if_index_loaded_correctly('summaries', indexer.index['summaries']))
+
+if __name__ == "__main__":
+    main()

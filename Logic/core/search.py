0efa562c9bbf8344098d9470cb050b167eea9ce7
Danial Gharib <danial.mail.gh@gmail.com>
2024-04-11 23:24:09 +0330

    utils completed.

--- Logic/core/search.py
+++ Logic/core/search.py
@@ -1,9 +1,9 @@
 import json
 import numpy as np
-from preprocess import Preprocessor
-from scorer import Scorer
-from indexer.indexes_enum import Indexes, Index_types
-from indexer.index_reader import Index_reader
+from .preprocess import Preprocessor
+from .scorer import Scorer
+from .indexer.indexes_enum import Indexes, Index_types
+from .indexer.index_reader import Index_reader
 
 
 class SearchEngine:

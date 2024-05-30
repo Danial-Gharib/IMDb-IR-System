19e115f6f10b90c7cf9a9f1de91db0d2baedc334
Danial-Gharib <danial.mail.gh@gmail.com>
2024-05-30 11:54:26 +0330

    classification/naive-bayes.py completed.

--- Logic/core/classification/naive_bayes.py
+++ Logic/core/classification/naive_bayes.py
@@ -2,9 +2,9 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-
-from .basic_classifier import BasicClassifier
-from .data_loader import ReviewLoader
+from tqdm import tqdm
+from basic_classifier import BasicClassifier
+from data_loader import ReviewLoader
 
 
 class NaiveBayes(BasicClassifier):
@@ -38,7 +38,22 @@ class NaiveBayes(BasicClassifier):
         self
             Returns self as a classifier
         """
-        pass
+        self.classes, counts = np.unique(y, return_counts=True)
+        self.num_classes = len(self.classes)
+        self.number_of_samples, self.number_of_features = x.shape
+
+        self.prior = counts / self.number_of_samples
+
+        self.feature_probabilities = np.zeros((self.num_classes, self.number_of_features))
+
+        for idx, cls in enumerate(tqdm(self.classes, desc="Fitting the NaiveBayes model")):
+            x_class = x[y == cls]
+            self.feature_probabilities[idx, :] = (np.sum(x_class, axis=0) + self.alpha) / (x_class.shape[0] + self.alpha * self.number_of_features)
+
+        self.log_probs = np.log(self.feature_probabilities)
+        self.log_prior = np.log(self.prior)
+
+        return self
 
     def predict(self, x):
         """
@@ -52,7 +67,12 @@ class NaiveBayes(BasicClassifier):
             Return the predicted class for each doc
             with the highest probability (argmax)
         """
-        pass
+        if isinstance(x, np.ndarray):
+            log_likelihood = np.dot(x, self.log_probs.T)
+        else:
+            log_likelihood = x.dot(self.log_probs.T)
+        log_likelihood += self.log_prior
+        return self.classes[np.argmax(log_likelihood, axis=1)]
 
     def prediction_report(self, x, y):
         """
@@ -67,13 +87,17 @@ class NaiveBayes(BasicClassifier):
         str
             Return the classification report
         """
-        pass
+        preds = self.predict(x)
+        return classification_report(y, preds)
 
     def get_percent_of_positive_reviews(self, sentences):
         """
         You have to override this method because we are using a different embedding method in this class.
         """
-        pass
+        transformed_sentences = self.cv.transform(sentences)
+        predictions = self.predict(transformed_sentences)
+        percent_positive = np.mean(predictions == "positive") * 100
+        return percent_positive
 
 
 # F1 Accuracy : 85%
@@ -83,4 +107,39 @@ if __name__ == '__main__':
     Finally, predict the test data and print the classification report
     You can use scikit-learn's CountVectorizer to find the embeddings.
     """
+    file_path = "classification_training/archive/IMDB Dataset.csv"
+    loader = ReviewLoader(file_path=file_path)
+    loader.load_data(train=False, model_path="classification_training/ft_model.bin", write_rs=False, save_model=False)
+    print("model loaded")
+    reviews, labels = loader.review_tokens, loader.sentiments
+    print("reviews and sentiments loaded")
 
+    reviews = [" ".join(review) for review in reviews]
+    print("review tokens made back to sentences")
+
+    x_train, x_test, y_train, y_test = train_test_split(reviews, labels, test_size=0.1, random_state=42)
+    print("Data has been split")
+
+    cv = CountVectorizer()
+    x_train_cv = cv.fit_transform(x_train)
+    x_test_cv = cv.transform(x_test)
+    print("Count vectors generated")
+
+    nb = NaiveBayes(count_vectorizer=cv)
+    nb.fit(x_train_cv, y_train)
+    print("Training Finished")
+
+    report = nb.prediction_report(x_test_cv, y_test)
+    print("Classification report\n", report)
+    
+
+    ##### results ########################
+#     Classification report
+#                precision    recall  f1-score   support
+
+#            0       0.88      0.85      0.86      2481
+#            1       0.85      0.89      0.87      2519
+
+#     accuracy                           0.87      5000
+#    macro avg       0.87      0.87      0.87      5000
+# weighted avg       0.87      0.87      0.87      5000
